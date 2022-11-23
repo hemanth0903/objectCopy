ee42a1ef90d0336924a34b343035debd536dc905
hemanth0903 <118755008+hemanth0903@users.noreply.github.com>
2022-11-23 18:19:10 +0530

    Update Employee.java

--- src/com/baeldung/java/deepcopy/Employee.java
+++ src/com/baeldung/java/deepcopy/Employee.java
@@ -1,19 +1,19 @@
 package com.baeldung.java.deepcopy;
 
-public class Employee implements Cloneable{
+public class Employee implements Cloneable {
 	
 	public int id;
 	public String name;
 	public Role role;
 	
-	Employee(int id, String name, Role role){
+	Employee(int id, String name, Role role) {
 		
 		this.id = id;
 		this.name = name;
 		this.role = role;
 	}
 	
-	protected Object clone() throws CloneNotSupportedException{
+	protected Object clone() throws CloneNotSupportedException {
 		
 		Employee e1 = (Employee)super.clone();
 		e1.role= (Role)role.clone();
