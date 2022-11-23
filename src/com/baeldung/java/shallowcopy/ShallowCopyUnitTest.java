e7c099d5687fb84a4b9a7eef84ed6aa2e996c9b9
hemanth0903 <118755008+hemanth0903@users.noreply.github.com>
2022-11-23 18:21:45 +0530

    Update ShallowCopyUnitTest.java

--- src/com/baeldung/java/shallowcopy/ShallowCopyUnitTest.java
+++ src/com/baeldung/java/shallowcopy/ShallowCopyUnitTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 class ShallowCopyUnitTest {
 
 	@Test
-	public void whenCreatingShallowCopy_thenObjectsShouldbeSame() throws CloneNotSupportedException{
+	public void whenCreatingShallowCopy_thenObjectsShouldbeSame() throws CloneNotSupportedException {
 		
           Role r1 = new Role(11,"Software Analyst",600000);
 	  Employee e1 = new Employee(001,"Hemanth",r1);
