1763a8ce74da42541fffeb9d6a093f15de1ec17c
hemanth0903 <118755008+hemanth0903@users.noreply.github.com>
2022-11-23 18:28:38 +0530

    Update DeepCopyUnitTest.java

--- src/com/baeldung/java/deepcopy/DeepCopyUnitTest.java
+++ src/com/baeldung/java/deepcopy/DeepCopyUnitTest.java
@@ -9,12 +9,12 @@ class DeepCopyUnitTest {
 	@Test
 	 public void whenCreatingDeepCopy_thenObjectsShouldNotbeSame() throws CloneNotSupportedException {
 		
-            Role r1 = new Role(11,"Software Analyst",600000);
-	    Employee e1 = new Employee(001,"Hemanth",r1);
+            Role r1 = new Role(11, "Software Analyst", 600000);
+	    Employee e1 = new Employee(001, "Hemanth", r1);
 	    Employee e2 = (Employee)e1.clone();	
 		
 	    e2.role.grossPay = 700000;
 		
-	    assertNotSame(e1.role.grossPay,e2.role.grossPay);
+	    assertNotSame(e1.role.grossPay, e2.role.grossPay);
     }
 }
