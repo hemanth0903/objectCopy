560cccfc63a2016629dfb301e6fc2399d2f34554
hemanth0903 <118755008+hemanth0903@users.noreply.github.com>
2022-11-23 18:37:23 +0530

    Update Employee.java

--- src/com/baeldung/java/shallowcopy/Employee.java
+++ src/com/baeldung/java/shallowcopy/Employee.java
@@ -2,13 +2,13 @@ package com.baeldung.java.shallowcopy;
 
 public class Employee implements Cloneable {
 	
-	public int e_id;
+	public int id;
 	public String name;
 	public Role role;
 	
-	Employee(int e_id, String name, Role role) {
+	Employee(int id, String name, Role role) {
 		
-		this.e_id = e_id;
+		this.id = id;
 		this.name = name;
 		this.role = role;
 	}
